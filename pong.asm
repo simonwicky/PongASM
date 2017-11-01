efe35aaabd087db4e9fb6ad95d6e0840cbbff88f
Simon Wicky <simon.wicky@epfl.ch>
2017-11-01 08:27:15 +0100

    added equ

---
+++ pong.asm
@@ -0,0 +1,5 @@
+.equ	BALL, 		0x1000		#ball state (position and velocity)
+.equ	PADDLES,	0x1010		#paddles position
+.equ	SCORES,		0x1018		#game scores
+.equ	LEDS,		0x2000		#LED addesses
+.equ	BUTTONS,	0x2030		#Button addresses 
