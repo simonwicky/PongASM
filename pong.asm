df94004b074b8c000564d668b413f55f0f69e2e5
Simon Wicky <simon.wicky@epfl.ch>
2017-11-08 09:39:47 +0100

    little correction

--- pong.asm
+++ pong.asm
@@ -6,27 +6,34 @@
 
 
 
-main:  addi  t0, zero, 0x2
-    stw    t0, PADDLES(zero)
-    stw    t0, PADDLES+4(zero)
-    addi  sp, zero, LEDS
-    addi  t0, zero, 0x2
-    stw    t0,  BALL(zero)
-    addi  t0, zero, 0x1
-    stw    t0, BALL +4(zero)
-    addi  t0, zero, -1
-    stw    t0, BALL +8(zero)
-    stw    t0, BALL +12(zero)
+#main:  addi  t0, zero, 0x2
+#    stw    t0, PADDLES(zero)
+#    stw    t0, PADDLES+4(zero)
+#    addi  sp, zero, LEDS
+#    addi  t0, zero, 0x2
+#    stw    t0,  BALL(zero)
+#    addi  t0, zero, 0x1
+#    stw    t0, BALL +4(zero)
+#    addi  t0, zero, -1
+#    stw    t0, BALL +8(zero)
+#    stw    t0, BALL +12(zero)
+#
+#loop:  call   clear_leds
+#    ldw    a0, BALL(zero)
+#    ldw    a1, BALL+4(zero)
+#    call   set_pixel
+#    call  draw_paddles
+#    call  hit_test
+#    call   move_ball
+#    call  move_paddles
+#    br    loop
 
-loop:  call   clear_leds
-    ldw    a0, BALL(zero)
-    ldw    a1, BALL+4(zero)
-    call   set_pixel
-    call  draw_paddles
-    call  hit_test
-    call   move_ball
-    call  move_paddles
-    br    loop
+main:  	addi  t0, zero, 0x5
+    	stw   t0, SCORES(zero)
+    	addi 	t0,t0,1
+    	stw    t0, SCORES+4(zero)
+    	call 	display_score
+	
 		
 
 
@@ -360,13 +367,15 @@ loop:  call   clear_leds
 		ldw 	t3, font_data(t1)					;t3 = word to display score 1
 		ldw 	t4, font_data(t2)					;t4 = word to display score 2
 
-		addi 	t6, zero, 17						;t6 = 17
+		addi 	t6, zero, 16						;t6 = 16
 		slli	t6, t6, 2							;t6 = t6*4
 		ldw 	t5, font_data(t6)					;t5 = words to display separator
 
 		stw		t3, LEDS(zero)						;store score 1 in the LEDS
 		stw		t4, LEDS+8(zero)					;store score 2 in the LEDS
 		stw		t5, LEDS+4(zero)					;store separator in the LEDS	
+
+		ret
 
 ; END:display_score
 
